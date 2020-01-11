5b6e1d2edfa89a31327cecf4fb06ecc97481871d
BryanHeYang <59769290+BryanHeYang@users.noreply.github.com>
2020-01-11 12:46:50 -0500

    Update floorplan.py

--- floorplan.py
+++ floorplan.py
@@ -1,3 +1,21 @@
 def class room:
-    listRoom
+    def __init__(self, oRooms = [], people = [], sensors = [], id = 0):
+        self.oRooms = oRooms
+        self.people = people
+        self.sensors = sensors
+        self.id = id
+
+    def seeRooms(self):
+        return self.oRooms
     
+    def seePeople(self):
+        return self.people
+    
+    def seeSensors(self):
+        return self.sensors
+
+    def roomid(self):
+        return self.id
+
+    
+
